72ec152ab92c0a65d91c5bf6cf1237454dfcc40e
HW5434 <l990628l@gmail.com>
2023-12-05 14:28:48 +0900

    문제 Test 3

---
+++ DAY02/src/DATA/Test03.java
@@ -0,0 +1,19 @@
+package DATA;
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		int 성인 = 12000;
+		int 학생 = 8500;
+		int 성인인원 = 2;
+		int 학생인원 = 2;
+	    int 성인Total = (성인 * 성인인원);
+		int 청소년Total = (학생 * 학생인원);
+		int 총인원요금 = (성인Total + 청소년Total);
+		
+		System.out.println(성인Total);
+		System.out.println(청소년Total);
+		System.out.println(총인원요금);
+	}
+
+}
