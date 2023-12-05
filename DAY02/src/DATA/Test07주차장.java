ebaf2152c36a132efcf80570e3d9f7f7348727ea
HW5434 <l990628l@gmail.com>
2023-12-05 19:15:42 +0900

    문제 Test07 : 주차장

---
+++ DAY02/src/DATA/Test07주차장.java
@@ -0,0 +1,29 @@
+package DATA;
+
+public class Test07주차장 {
+
+	public static void main(String[] args) {
+        //요금
+		int pay = 1500;
+		
+		//들어올때
+		int inhour = 11;
+		int inminute = 50;
+		//나갈때
+		int outhour =17;
+		int outminute = 25;
+		//분으로 바꾸기
+		int intime = inhour * 60 + inminute;
+		int outtime = outhour * 60 + outminute;
+		int totaltime = outtime - intime;
+		
+		int totalHour = totaltime / 60;
+		int totalMinute = totaltime % 60;
+		
+		System.out.println(totalHour);	
+		System.out.println(totalMinute);
+		int parkingtimepay = (totaltime / 10) * pay;
+		System.out.println(parkingtimepay);
+	}
+
+}
