6129195f56363e3e8b6ed899a14d496fa8190151
HW5434 <l990628l@gmail.com>
2023-12-05 12:54:08 +0900

    이름 출력 문제 풀이

---
+++ DAY02/src/welcome/Imformation.java
@@ -0,0 +1,8 @@
+package welcome;
+
+public class Imformation {
+	public static void main (String[] args) {
+		System.out.println("김현우");
+		System.out.println("(디지털컨버전스)공공데이터 융합 웹 애플리케이션 개발자 양성과정7");
+	}
+}
