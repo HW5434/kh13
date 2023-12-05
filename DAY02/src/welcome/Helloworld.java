4fd2a968b22d05981adf83fda90913ef0d94b1fc
HW5434 <l990628l@gmail.com>
2023-12-05 11:20:49 +0900

    코드 설명 추가

--- DAY02/src/welcome/Helloworld.java
+++ DAY02/src/welcome/Helloworld.java
@@ -1,8 +1,8 @@
 package welcome;
 
-public class Helloworld {
-	public static void main(String[] args) {
-		System.out.println("Hello java!");
+public class Helloworld {//파일을 대표하는 영역
+	public static void main(String[] args) {//메인 메소드
+		System.out.println("Hello java!");//출력 명령
 		//System.out.println("안녕 자바");
 	}
 }
