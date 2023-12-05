0033922ad0a7d3820a68c8fa677fe13f21bc947d
HW5434 <l990628l@gmail.com>
2023-12-05 14:32:32 +0900

    문제 Test01

--- DAY02/src/DATA/Test01.java
+++ DAY02/src/DATA/Test01.java
@@ -13,7 +13,7 @@ public class Test01 {
 		System.out.println(2023-12-05); //식의 결과를 출력
 		
 		//(Q) 식권이 한장에 7000원 일때 식권 10장은 얼마?
-		//System.out.println(70000);//나의 재롱잔치
+		//System.out.println(70000);//나의 재롱잔치 ㅋ
 		System.out.println(7000*10);//컴퓨터에게 시킨것
 	}
 }
