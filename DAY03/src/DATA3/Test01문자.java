44ba71b86bdc1569332224d87b08fe2343139e4c
HW5434 <l990628l@gmail.com>
2023-12-06 17:16:24 +0900

    Test01문자 : 자바의 문자

---
+++ DAY03/src/DATA3/Test01문자.java
@@ -0,0 +1,21 @@
+package DATA3;
+
+public class Test01문자 {
+
+	public static void main(String[] args) {
+		//자바의 문자 표현 방식
+		//- 자바는 유니코드셋을 표현할 수 있따
+		//- char 형태로 표현
+		//-글자이지만 숫자로 변환이 가능
+		
+		char a = '힣';
+		System.out.println(a);
+		
+		int b = a;
+		System.out.println(b);
+		
+		char c = 44032;
+		System.out.println(c);
+	}
+
+}
