3ca5c0d3363128bf5403731e8ee4eddc057c1d06
HW5434 <l990628l@gmail.com>
2023-12-06 10:56:35 +0900

    문제 Test08 : 나눗셈

--- DAY03/src/DATA/Test08나눗셈.java
+++ DAY03/src/DATA/Test08나눗셈.java
@@ -3,8 +3,23 @@ package DATA;
 public class Test08나눗셈 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int number = 75;
+		
+		//위 숫자의 10의자리, 1의자리 숫자를 각각 출력
+		System.out.println(number / 10);
+		System.out.println(number % 10);
+		
+		
+		//숫자의 100의자리, 10의자리 , 1의자리를 각각 출력
+		int number2 = 139;
+		
+		System.out.println(number2 / 100);
+		int number02 = number2 % 100;
+		System.out.println(number02 / 10);
+		System.out.println(number2 / 10 % 10);
+		System.out.println(number02 % 10);
+		
+		
 	}
 
 }
