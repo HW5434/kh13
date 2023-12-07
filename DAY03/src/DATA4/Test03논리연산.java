4111b5e6200bfbccc5600aaef2844b91679a3a59
HW5434 <l990628l@gmail.com>
2023-12-07 12:29:43 +0900

    Test03

--- DAY03/src/DATA4/Test03논리연산.java
+++ DAY03/src/DATA4/Test03논리연산.java
@@ -3,7 +3,16 @@ package DATA4;
 public class Test03논리연산 {
 
 	public static void main(String[] args) {
-
+		//논리 연산
+		//-&&(and) , ||(or) 을 이용하여 조합
+		
+		//ex 19살은 청소년인가요?
+		//-청소년은 14살부터 19살까지 입니다,
+		
+		int age = 13;
+		//boolean teen = 14 <= age <= 19;
+		boolean teen = 14 <= age && age <= 19;
+		System.out.println("청소년인가요?" + teen);
 	}
 
 }
