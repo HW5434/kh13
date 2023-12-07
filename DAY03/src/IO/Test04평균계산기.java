4d0a3d8ee99da476bf5d7116f2c30fd2e5b871a7
HW5434 <l990628l@gmail.com>
2023-12-07 17:11:29 +0900

    Test

--- DAY03/src/IO/Test04평균계산기.java
+++ DAY03/src/IO/Test04평균계산기.java
@@ -9,14 +9,18 @@
 	//	수학 점수(math)
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		int korean = sc.nextInt();
-		int english = sc.nextInt();
-		int math = sc.nextInt();
+		Scanner sc = new Scanner(System.in);	
+		System.out.print("국어점수 : ");
+		double korean = sc.nextDouble();
+		System.out.print("영어점수 : ");
+		double english = sc.nextDouble();
+		System.out.print("수학점수 "); //println 에서 ln을 빼면 옆으로 출력됌
+		double math = sc.nextDouble();
 		
-		int total = (korean + english + math);
-		int result = total / 3;
+		double total = (korean + english + math);
+		double result = total / 3;//평균은 더블쓰기 평균 : average
+		
 		
 		System.out.println("평균점수는 " + result + "점 이고" + " 총점은 " + total + "입니다.");
 	}
