49c2b6523723d69cf36b2717b94a881a4ec72aae
HW5434 <l990628l@gmail.com>
2023-12-07 16:04:52 +0900

    Test 01 출력

---
+++ DAY03/src/IO/Test01출력.java
@@ -0,0 +1,21 @@
+package IO;
+
+public class Test01출력 {
+
+	public static void main(String[] args) {
+		
+		//표준 출력 장치 : ex 모니터 out
+		//표준 입력 장치 : ex 키보드 in
+		
+		//표준 출력 통로(System.out)
+		//-시스템의 기본 출력 장치를 이용할 수 있도록 하는 도구
+		//-println 명령은 글자 출력 후 줄바꿈(개행)을 수행 = ln은 line
+		//-print 명령은 글자만 출력
+		System.out.println("Hello");
+		System.out.print("J");
+		System.out.print("A");
+		System.out.print("V");
+		System.out.print("A");
+	}
+
+}
