cdb698ae98884ecbd4627da7e4cd0c0c2f62d02a
HW5434 <152838242+HW5434@users.noreply.github.com>
2023-12-29 17:14:46 +0900

    scanner 정리하기

--- day19/src/api/util/scanner/Test02문제되는상황.java
+++ day19/src/api/util/scanner/Test02문제되는상황.java
@@ -7,10 +7,11 @@ public class Test02문제되는상황 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("이름 : ");
-		String name = sc.next();
+		String name = sc.next(); //단어 입력(띄어쓰기 , 엔터 , 탭 등)
+		sc.nextLine();//남아있는 \n을 , 정리 충돌 방지
 		
-		System.out.println("이름 : ");
-		String intro = sc.next();
+		System.out.println("자기소개 : ");
+		String intro = sc.nextLine(); //한줄 입력(엔터를 삭제시켜준다)
 		
 		sc.close();
 		
