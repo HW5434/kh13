269f8f9c159fb3bd11da8a5e5804c020bf610d2b
HW5434 <152838242+HW5434@users.noreply.github.com>
2024-01-10 12:50:23 +0900

    1차 세팅

---
+++ jdbc03/src/jdbc/dto/MenuDto.java
@@ -0,0 +1,46 @@
+package jdbc.dto;
+
+public class MenuDto {
+
+	private int menuNo;
+	private String menuNameKr;
+	private String menuNameEg;
+	private String menuType;
+	private int munuPrice;
+	
+	
+	public MenuDto() {
+		super();
+	}
+	
+	public int getMenuNo() {
+		return menuNo;
+	}
+	public void setMenuNo(int menuNo) {
+		this.menuNo = menuNo;
+	}
+	public String getMenuNameKr() {
+		return menuNameKr;
+	}
+	public void setMenuNameKr(String menuNameKr) {
+		this.menuNameKr = menuNameKr;
+	}
+	public String getMenuNameEg() {
+		return menuNameEg;
+	}
+	public void setMenuNameEg(String menuNameEg) {
+		this.menuNameEg = menuNameEg;
+	}
+	public String getMenuType() {
+		return menuType;
+	}
+	public void setMenuType(String menuType) {
+		this.menuType = menuType;
+	}
+	public int getMunuPrice() {
+		return munuPrice;
+	}
+	public void setMunuPrice(int munuPrice) {
+		this.munuPrice = munuPrice;
+	}
+}
---
+++ jdbc03/src/jdbc/mapper/EmpMapper.java
@@ -0,0 +1,23 @@
+package jdbc.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import jdbc.dto.EmpDto;
+
+public class EmpMapper implements RowMapper<EmpDto> {
+
+	@Override
+	public EmpDto mapRow(ResultSet rs, int idx) throws SQLException {
+		EmpDto dto = new EmpDto();
+		dto.setEmpNo(rs.getInt("emp_no"));
+		dto.setEmpName(rs.getNString("emp_name"));
+		dto.setEmpDept(rs.getNString("emp_dept"));
+		dto.setEmpDate(rs.getNString("emp_date"));
+		dto.setEmpSal(rs.getInt("emp_sal"));
+		return dto;
+		
+	}
+}
