5969c3b23e6d73151da66007734036fab4316d15
HW5434 <152838242+HW5434@users.noreply.github.com>
2024-01-12 16:27:09 +0900

    Spring01

--- jdbc04/src/jdbc/dao/MemberDao.java
+++ jdbc04/src/jdbc/dao/MemberDao.java
@@ -10,46 +10,53 @@ import jdbc.util.JdbcHelper;
 
 //member 테이블 데이터 처리를 담당하는 클래스
 public class MemberDao {
-//메소드에서 공용으로 사용하는 도구들을 생성
+	//메소드에서 공용으로 사용하는 도구들을 생성
 	private JdbcTemplate jdbcTemplate = JdbcHelper.getJdbcTemplate();
 	private MemberMapper mapper = new MemberMapper();
 	
-	//가입(등록 , Create)
+	//가입(등록, Create)
 	public void insert(MemberDto dto) {
 		String sql = "insert into member("
-				+ "member_id, member_pw, member_nick, "
-				+ "member_birth, member_contact, member_email, "
-				+ "member_post, member_address1, member_address2"
-				+ ") values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
-				Object[] data = {
-						dto.getMemberId(), dto.getMemberPw(),
-						dto.getMemberNick(), dto.getMemberBirth(),
-						dto.getMemberContact(),
-						dto.getMemberEmail(), dto.getMemberPost(),
-						dto.getMemberAddress1(), dto.getMemberAddress2()
-				};
-				jdbcTemplate.update(sql, data);
-	}
-	
-	//목록(조회 , Read)
-	public List<MemberDto> selectList(){
-		String sql = "select * from member order by member_id asc";
-		return jdbcTemplate.query(sql, mapper);
+							+ "member_id, member_pw, member_nick, "
+							+ "member_birth,member_contact, member_email, "
+							+ "member_post, member_address1, member_address2"
+						+ ") values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//		String sql = "insert into member("
+//							+ "member_id, member_pw, member_nick, "
+//							+ "member_birth,member_contact, member_email, "
+//							+ "member_post, member_address1, member_address2,"
+//							+ "member_level, member_point, member_join, member_login"
+//						+ ") values(?, ?, ?, ?, ?, ?, ?, ?, ?, '일반회원', 0, sysdate, null)";
+		Object[] data = {
+			dto.getMemberId(), dto.getMemberPw(), dto.getMemberNick(),
+			dto.getMemberBirth(), dto.getMemberContact(), dto.getMemberEmail(),
+			dto.getMemberPost(), dto.getMemberAddress1(), dto.getMemberAddress2()
+		};
+		jdbcTemplate.update(sql, data);
 	}
 
-	//상세(조회 , )
-	public MemberDto selectOne(String MemberId) {
+	//목록(조회, Read)
+	public List<MemberDto> selectList() {
+		String sql = "select * from member order by member_id asc";
+		//Object[] data = {};
+		return jdbcTemplate.query(sql, mapper);
+	}
+	//검색(조회, Read) 오버로딩
+	public List<MemberDto> selectList(String column, String keyword) {
+		String sql = "select * from member where instr("+column+", ?) > 0 "
+										+ "order by "+column+" asc, member_id asc";
+		Object[] data = {keyword};
+		return jdbcTemplate.query(sql, mapper, data);
+	}
+
+	//상세(조회, Read)
+	public MemberDto selectOne(String memberId) {
 		String sql = "select * from member where member_id = ?";
-		Object[] data = {MemberId};
+		Object[] data = {memberId};
 		List<MemberDto> list = jdbcTemplate.query(sql, mapper, data);
-//		return list.isEmpty() ? null : list.get(0);
-		if(list.isEmpty()) {
-				return null;
-			}
-			else {
-				return list.get(0);
-			}
-		}
+		return list.isEmpty() ? null : list.get(0);
+	}
+
 
 	//비밀번호 변경(수정, Update)
 	public boolean updateMemberPw(MemberDto dto) {
@@ -57,4 +64,16 @@ public class MemberDao {
 		Object[] data = {dto.getMemberPw(), dto.getMemberId()};
 		return jdbcTemplate.update(sql, data) > 0;
 	}
+
+	public boolean delete(String memberId) {
+		String sql = "delete member where member_id = ?";
+		Object[] data = {memberId};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
 }
+
+
+
+
+
+
