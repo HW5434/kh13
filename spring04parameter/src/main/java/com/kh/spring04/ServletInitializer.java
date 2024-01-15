af3c41f5fe76c1ff92723614abc20bc035c7c0a3
HW5434 <152838242+HW5434@users.noreply.github.com>
2024-01-15 13:34:23 +0900

    Spring 05

--- spring04parameter/src/main/java/com/kh/spring04/ServletInitializer.java
+++ spring04parameter/src/main/java/com/kh/spring04/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.kh.spring04parameter;
+package com.kh.spring04;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
