ceb89a25b14880588b9561ee38f9781363bd8bfe
HW5434 <152838242+HW5434@users.noreply.github.com>
2024-01-16 16:56:30 +0900

    porejct Spring 7 share

---
+++ spring07student/src/test/java/com/kh/spring07student/Spring07studentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.spring07student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07studentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
