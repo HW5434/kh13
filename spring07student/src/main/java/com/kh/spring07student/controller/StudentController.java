7f36b4c736de5ca413813a782495d03dd546e253
HW5434 <152838242+HW5434@users.noreply.github.com>
2024-01-17 09:55:32 +0900

    spring 07 풀이

--- spring07student/src/main/java/com/kh/spring07student/controller/StudentController.java
+++ spring07student/src/main/java/com/kh/spring07student/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.kh.spring07student.controller;
 
+import java.text.DecimalFormat;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.spring07student.dao.StudentDao;
@@ -30,7 +34,46 @@ public class StudentController {
 			return "점수를 찾을 수 없습니다.";
 		}
 	}
+	@RequestMapping("/list")
+	public String list(
+			@RequestParam(required = false) String column,
+			@RequestParam(required = false) String keyword) {
+		boolean isSearch = column != null && keyword != null;
+		List<StudentDto> list = isSearch ? dao.selectList(column, keyword) : dao.selectList();
 		
+		StringBuffer buffer = new StringBuffer();
+		for(StudentDto dto : list) {
+			buffer.append("[");
+			buffer.append(dto.getStudentId());
+			buffer.append("]");
+			buffer.append(dto.getStudentName());
+			buffer.append(" - ");
+			buffer.append("국어-"+dto.getKoreanScore());
+			buffer.append(",수학-"+dto.getMathScore());
+			buffer.append(",영어-"+dto.getEnglishScore());
+			buffer.append(" (");
+			buffer.append(")");
+			buffer.append("<br>");
+		}
+		return buffer.toString();
+	}
+	@RequestMapping("/detail")
+	public String detail(@RequestParam int studentId) {
+		StudentDto dto = dao.selectOne(studentId);
+		DecimalFormat fmt = new DecimalFormat("#,##0.00");
+		if(dto != null) {
+			StringBuffer buffer = new StringBuffer();
+			buffer.append("학생번호 - " + dto.getStudentId() + "<br>");
+			buffer.append("학생명 - " + dto.getStudentName() + "<br>");
+			buffer.append("국어점수 - " + dto.getKoreanScore() + "점<br>");
+			buffer.append("수학점수 - " + dto.getMathScore() + "점<br>");
+			buffer.append("영어점수 - " + dto.getEnglishScore() + "점<br>");
+			return buffer.toString();
+		}
+		else {
+			return "존재하지 않는 학생 번호";
+		}
 		
 
+	}
 }
--- spring07student/src/main/java/com/kh/spring07student/dao/StudentDao.java
+++ spring07student/src/main/java/com/kh/spring07student/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.kh.spring07student.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -43,11 +45,29 @@ public class StudentDao {
 		return jdbcTemplate.update(sql, data) > 0;
 	}
 	
-//	public boolean delete(int studentId) {
-//		String sql = "delete student where student_id = ? > 0";
-//		Object[] data = {};
-////		if
-//	}
-	
+	public List<StudentDto> selectList() {
+		String sql = "select * from student order by student_id asc";
+		return jdbcTemplate.query(sql, mapper);
+	}
+	public List<StudentDto> selectList(String column, String keyword) {
+		String sql = "select * from student where instr(" + column+", ?) > 0 "
+											+ "order by "+column+" asc, student_id asc";
+		Object[] data = {keyword};
+		return jdbcTemplate.query(sql, mapper, data);
+	}
+	public StudentDto selectOne(int studentId) {
+		String sql = "select * from student where student_id = ?";
+		Object[] data = {studentId};
+		List<StudentDto> list = jdbcTemplate.query(sql, mapper, data);
+		return list.isEmpty() ? null : list.get(0);
+	}
+
+	public boolean delete(int studentId) {
+		String sql = "delete student where student_id = ?";
+		Object[] data = {studentId};
+		return jdbcTemplate.update(sql, data) > 0;
+	}
 }
+	
+
 
