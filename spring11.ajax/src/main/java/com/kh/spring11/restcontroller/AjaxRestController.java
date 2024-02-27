7813024fe8134e84762ee3987d76e8859b8c5ad2
HW5434 <l990628l@gmail.com>
2024-02-27 11:02:49 +0900

    회원 아이디 중복 검사 테스트

--- spring11.ajax/src/main/java/com/kh/spring11/restcontroller/AjaxRestController.java
+++ spring11.ajax/src/main/java/com/kh/spring11/restcontroller/AjaxRestController.java
@@ -1,16 +1,35 @@
 package com.kh.spring11.restcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kh.spring11.dao.MemberDao;
+import com.kh.spring11.dto.MemberDto;
 
 @CrossOrigin
 @RestController
 public class AjaxRestController {
 
+	@Autowired
+	private MemberDao memberDao;
+	
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Ajax!";
 	}
 	
+	@RequestMapping("/checkId")
+	public String checkId(@RequestParam String memberId) {
+		MemberDto memberDto = memberDao.selectOne(memberId);
+		
+		if(memberDto == null) {
+			return "NNNNY"; //사용 가능한 경우 (DB에 없는 경우)
+		}
+		else {
+			return "NNNNN"; //사용 불가능한 경우(DB에 있는 경우)			
+		}
+	}
 }
