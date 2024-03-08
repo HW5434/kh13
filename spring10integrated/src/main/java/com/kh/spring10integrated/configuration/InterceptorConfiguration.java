0f8ed17bd1a519c7e459fcb0877e171b4deeab8d
HW5434 <l990628l@gmail.com>
2024-03-08 09:40:10 +0900

    수정필요

--- spring10integrated/src/main/java/com/kh/spring10integrated/configuration/InterceptorConfiguration.java
+++ spring10integrated/src/main/java/com/kh/spring10integrated/configuration/InterceptorConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.kh.spring10integrated.interceptor.AdminIntersepter;
+import com.kh.spring10integrated.interceptor.BoardOwnerInterceptor;
 import com.kh.spring10integrated.interceptor.BoardReadcountInterceptor;
 import com.kh.spring10integrated.interceptor.Memberinterceptor;
 import com.kh.spring10integrated.interceptor.TestInterceptor;
@@ -18,23 +19,26 @@ import com.kh.spring10integrated.interceptor.TestInterceptor;
 public class InterceptorConfiguration implements WebMvcConfigurer {
 	@Autowired
 	private TestInterceptor testInterceptor;
-	
+
 	@Autowired
 	private Memberinterceptor memberInterceptor;
-	
+
 	@Autowired
 	private AdminIntersepter adminInterceptor;
-	
+
 	@Autowired
 	private BoardReadcountInterceptor boardReadcountInterceptor;
-	
+
+	@Autowired
+	private BoardOwnerInterceptor boardOwnerInterceptor;
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 //		registry에다가 인터셉터를 주소와 함께 등록
 //		registry.addInterceptor(인터셉터객체).addPathPatterns(주소);
 //		registry.addInterceptor(testInterceptor)
 //					.addPathPatterns("/pocketmon/list", "/emp/list");
-		
+
 //		지정한 페이지만 허용하도록 설정
 //		registry.addInterceptor(memberInterceptor)
 //					.addPathPatterns(
@@ -43,33 +47,30 @@ public class InterceptorConfiguration implements WebMvcConfigurer {
 //							"/member/change",
 //							"/member/exit"
 //					);
-		
+
 //		중간주소를 등록하고 특정 페이지를 제외(exclude)
 //		- 주소를 설정할 때 **와 *를 사용할 수 있다
 //		- **는 하위 엔드포인트까지 모두 포함하여 설정할 때 사용 (후손)
 //		- *는 동일 엔드포인트까지만 포함하여 설정할 때 사용 (자식)
 		registry.addInterceptor(memberInterceptor)
-					.addPathPatterns("/member/**")
-					.excludePathPatterns(
-							//"/member/join", "/member/joinFinish",
-							"/member/**",
-							"/board/**",
-							"/point/**",
-							"/rest/board_like/toggle"
-					);
-		
-		//관리자 인터셉터 등록
-		//registry.addInterceptor(adminInterceptor)
-		//			.addPathPatterns("/admin/**");
-		
-		//게시글 조회수 중복방지 인터셉터 등록
+										.addPathPatterns("/member/**").excludePathPatterns(
+				// "/member/join", "/member/joinFinish",
+				"/member/**", 
+				"/board/**", 
+				"/point/**", 
+				"/rest/board_like/toggle"
+				);
+
+		// 관리자 인터셉터 등록
+		 registry.addInterceptor(adminInterceptor)
+		 									.addPathPatterns("/admin/**");
+
+		// 게시글 조회수 중복방지 인터셉터 등록
 		registry.addInterceptor(boardReadcountInterceptor)
-					.addPathPatterns("/board/detail");
+											.addPathPatterns("/board/detail");
+		//내글 또는 관리자만 수정 삭제하는 인터셉터 등록
+		//registry.addInterceptor(boardOwnerInterceptor).addPathPatterns("")
+		
+		
 	}
 }
-
-
-
-
-
-
