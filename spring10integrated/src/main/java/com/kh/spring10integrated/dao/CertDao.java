0264e48712b081466f2fc0b4c11f0e4470114770
HW5434 <l990628l@gmail.com>
2024-03-08 16:47:00 +0900

    스케쥴러를 이용한 인증번호 삭제

--- spring10integrated/src/main/java/com/kh/spring10integrated/dao/CertDao.java
+++ spring10integrated/src/main/java/com/kh/spring10integrated/dao/CertDao.java
@@ -45,6 +45,12 @@ public class CertDao {
 		return list.isEmpty() ? false : true;
 		//return !list.isEmpty(); 비어있지 않으면 성공
 	}
+	
+	//5분이 지난 인증번호들을 삭제하는 메소드를 생성
+	public boolean deleteLegacy() {
+		String sql = "delete cert where cert_time < sysdate-5/24/60";
+		return jdbcTemplate.update(sql) > 0;
+	}
 }
 
 
